9c2777e2b121730b967f78462a7f3d3855ba2bef
jmccollum-bw <jmccollum@burwood.com>
2018-10-24 10:09:15 -0500

    initial commit

---
+++ Build-ESXiInstallIso.ps1
@@ -0,0 +1,116 @@
+﻿
+<#
+.SYNOPSIS
+
+This script will create ESXi installer ISOs for automated installing of ESXi  
+.DESCRIPTION
+
+The goal of this script is to help in the deployment of ESXi in an automated manor that doesn't require DHCP. 
+This script will take an Excel file that lists the specified Hosts and their network configuration and a source ESXi ISO which are then used to created 
+named ESXi ISOs for every host that needs ESXi installed on them. Each of the generated ISOs has a KS file and a replaced BOOT.CFG file that will auto 
+run the KS file that is specified. These ISOs can be mounted to each host individually, or in the case of UCS Manager, a KVM policy can be put in place, 
+to auto mount every Service Profile to the specific ISO that matches the Service Profile Name. 
+This allows for rapid, automated deployment of ESXi without the need of DHCP. 
+
+There is a manual step to this process that isn't automated yet, please see the readme.md in the github repo. 
+
+ImportExcel Module is required for this script. https://github.com/dfinke/ImportExcel
+The linux iso cmdline tools growisofs is also required and is packaged in the repo. 
+
+
+.PARAMETER ImportFile
+The Excel file to be imported
+
+.PARAMETER ISO
+The ISO file to use as the source for the script. 
+
+.EXAMPLE
+ .\Build-ESXiInstallISO.ps1 -ImportFile .\vSphereDesign.xlsx -ISO ESXi.6.7
+
+#>
+
+##############################################
+#region Parameters
+##############################################
+
+Param(
+
+    [Parameter(Position = 0,
+        Mandatory = $true,
+        HelpMessage = 'The config file to use')]
+    [ValidateNotNullOrEmpty()]	
+    [string]$ImportFile,
+
+    [Parameter(Position = 1,
+        Mandatory = $true,
+        HelpMessage = 'The ISO to use')]
+    [ValidateNotNullOrEmpty()]		
+    [string]$ISO	
+)
+
+#endregion
+
+##############################################
+#region Script
+##############################################
+#Requires –Modules ImportExcel
+
+#import the Excel file
+$Sheets = Get-ExcelSheetInfo -Path $ImportFile
+
+#read every sheet into a hash table
+foreach ($Sheet in $Sheets) {
+    New-Variable -Name ("VMData" + $($Sheet.name)) -Value (Import-Excel -Path $ImportFile -WorkSheetname $($Sheet.name))
+}
+
+#process through each host in the sheet. 
+foreach ($vmhost in $VMDataHosts) {
+
+    #removes any leftover KS.KS files
+    $ExistingKSFile = "Files\KS.KS"
+    if (Test-Path $ExistingKSFile) {
+        Remove-Item $ExistingKSFile
+    }
+
+    #checks if there is a management vlan configured in the design
+    if ($vmhost.ESXiMgmtVlan) {
+
+        $NetworkInfo = "network --bootproto=static --device=vmnic0 --nameserver=" + $vmhost.DNS + " --netmask=" + $vmhost.ESXiMgmtSubnet + " --gateway=" + $vmhost.ESXiMgmtGateway + " --vlanid=" + $vmhost.ESXiMgmtVlan + " --addvmportgroup=0 --ip=" + $vmhost.ESXiMgmtIP + " --hostname=" + $vmhost.Hostname + "`n" 
+    }
+    else {
+
+        $NetworkInfo = "network --bootproto=static --device=vmnic0 --nameserver=" + $vmhost.DNS + " --netmask=" + $vmhost.ESXiMgmtSubnet + " --gateway=" + $vmhost.ESXiMgmtGateway + " --addvmportgroup=0 --ip=" + $vmhost.ESXiMgmtIP + " --hostname=" + $vmhost.Hostname + "`n" 
+    }
+
+    #top of the KS file. Using Linux line endings
+    $FileContents = "# Auto install `n" +
+    "# Accept the VMware End User License Agreement`n" +
+    "vmaccepteula`n" +
+    "# Set the root password for the DCUI and Tech Support Mode`n" +
+    "rootpw " + $vmhost.RootPassword + "`n" + 
+    "# The install media is in the CD-ROM drive`n" +
+    "install --firstdisk --overwritevmfs`n" + 
+    $NetworkInfo +
+    "reboot"
+    
+    #export the file contents to the KS file
+    $FileContents | Out-File -FilePath Files\KS.CFG -Encoding utf8 -Force
+
+    #copies the source ISO to the current dir
+    Copy-Item $ISO -Destination "$($vmhost.Hostname).iso"
+    
+    #manual sleep process. This is for the growisofs, as I encountered race issues if I called it too quickly. 
+    Start-Sleep -Seconds 4
+
+    #Adds the contents of the Files dir, to the ISO. This will overwrite any files. 
+    .\growisofs.exe -M "$($vmhost.Hostname).iso" -R -J  .\Files
+    
+    #manual sleep process. This is for the growisofs, as I encountered race issues if I called it too quickly.
+    Start-Sleep -Seconds 4
+
+}
+
+##############################################
+#endregion
+##############################################
+#end of script
---
+++ readme.md
@@ -0,0 +1,41 @@
+##ESXi Full Auto Scripted Install Readme
+
+### What it is
+
+Automation ESXi deployments without DHCP is a pain to say the least. The only way I know how to do it, without DHCP, 
+is to use a unique KS file per ISO with the static IPs, and hostnames defined. This script will create a KS file per host
+and then load that KS file onto a ISO just for that host. This does result in having a ISO per host that is being installed.
+These ISOs can be mounted to each host individually, or in the case of UCS Manager, a KVM policy can be put in place, 
+to auto mount every Service Profile to the specific ISO that matches the Service Profile Name, which allows for 
+fully automated ESXi installations without any interaction! 
+
+Note: There is a setup task that has to be done manually. I haven't figured out a way to make this work on Windows, 
+without some extra apps installed, like 7-zip. 
+
+
+### Process
+
+This script will take an Excel file that lists the specified Hosts and their network configuration and a source ESXi ISO which are then used to created 
+named ESXi ISOs for every host that needs ESXi installed on them. Each of the generated ISOs has a KS file and a replaced BOOT.CFG file that will auto 
+run the KS file that is specified. 
+This allows for rapid, automated deployment of ESXi without the need of DHCP. 
+
+### Setup
+
+There is a manual step to this process that isn't automated yet which is modifying the source ISO's BOOT.CFG file to specify to run the KS file. 
+Every ESXi ISO has a unique BOOT.CFG file, as this file specifies the modules to be loaded, which change with every ESXi version, and the specific vendor
+edition of ESXi that is going to be installed. 
+
+For this step, open up your source ISO and copy the BOOT.CFG file to the Files directory. 
+change this line kernelopt= to 
+kernelopt=ks=cdrom:/KS.CFG 
+and safe the file
+
+The manual setup is done now and the script can be run. 
+
+Note: failing to do this step, will result in ESXi not installing at all or having issues. A sample BOOT.CFG is included in the files dir, but always use your own. 
+
+### acknowledgments
+Thanks to William Lang and his post [Virtually Ghetto](http://www.virtuallyghetto.com/2011/05/semi-interactive-automated-esxi.html) and a bunch of others that 
+helped me write this script and figure out this process on Windows. 
+
